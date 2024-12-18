57673a5185d27a99c3d94aa4add1763842103dac
Sheen Xavier <136368040+SheenXavier@users.noreply.github.com>
2024-12-18 11:04:31 +0530

    Adding gitignore

--- obj/Debug/net8.0/PointsTable.AssemblyInfo.cs
+++ obj/Debug/net8.0/PointsTable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PointsTable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731b96421dabbd6d6aa24b73606f6a4c192e757b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PointsTable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PointsTable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
